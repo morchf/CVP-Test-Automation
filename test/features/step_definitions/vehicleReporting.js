7fc80f52267de52699e7d2fff471a82d4af30870
Eden Morcho <eden.morcho@gtt.com>
2018-01-17 11:47:47 -0600

    added vehicle report blank steps

---
+++ test/features/step_definitions/vehicleReporting.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var {defineSupportCode} = require('cucumber');
+var {By, until, Key} = require('selenium-webdriver');
+var {expect} = require('chai');
+
+defineSupportCode(function({When, Then}) {
+
+
+
+  Then('I click on the vehicle report link', function (callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback();
+     });
+
+
+
+     Then('the vehicle report overflow display', function (callback) {
+              // Write code here that turns the phrase above into concrete actions
+              callback();
+            });
+
+
+            Then('I select the first Vehicle', function (callback) {
+                     // Write code here that turns the phrase above into concrete actions
+                     callback();
+                   });
+
+
+
+
+
+
+
+
+});
